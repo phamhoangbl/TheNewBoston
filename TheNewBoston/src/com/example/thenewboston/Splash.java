5c1f60981744530ec918e869b66bb08ce3858056
John Hoang <john.hoang.contact@gmail.com>
2014-06-10 18:42:41 +0700

    Add Splash (background, sound)

---
+++ TheNewBoston/src/com/example/thenewboston/Splash.java
@@ -0,0 +1,51 @@
+package com.example.thenewboston;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	MediaPlayer splashSong;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		//splash music
+		splashSong = MediaPlayer.create(Splash.this, R.raw.splash_sound);
+		splashSong.start();
+		
+		Thread timer = new Thread(){
+			@Override
+			public void run(){
+				try{
+					sleep(500);
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+				finally{
+					//Intent main = new Intent(getApplicationContext(), MainActivity.class);
+					Intent main = new Intent("android.intent.action.SplashJohn");
+					startActivity(main);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		splashSong.release();
+//		AlertDialog alertDialog = new AlertDialog.Builder(Splash.this).create();
+//		alertDialog.setTitle("On Pause");
+//		alertDialog.setMessage("Pause for test crile life");
+	}
+	
+}
