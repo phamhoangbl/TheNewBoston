d0d2a976bab094b7e9f64ac27dc709df1d0bb681
John Hoang <john.hoang.contact@gmail.com>
2014-06-15 23:10:23 +0700

    Add MenuInflate

--- TheNewBoston/src/com/example/thenewboston/Menu.java
+++ TheNewBoston/src/com/example/thenewboston/Menu.java
@@ -3,6 +3,7 @@ package com.example.thenewboston;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -31,5 +32,14 @@ public class Menu extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1 , _classes));
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.cool_menu, menu);
+		return true;
+	}
+	
 	
 }
