66e6fe65dc7445978759820d0935a420a0ae2341
John Hoang <john.hoang.contact@gmail.com>
2014-06-18 23:53:24 +0700

    Assets folder, typeface

--- TheNewBoston/src/com/example/thenewboston/MyGraphic.java
+++ TheNewBoston/src/com/example/thenewboston/MyGraphic.java
@@ -7,7 +7,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.BitmapFactory.Options;
 import android.graphics.Paint;
+import android.graphics.Paint.Align;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 public class MyGraphic extends View {
@@ -15,16 +17,22 @@ public class MyGraphic extends View {
 	Bitmap ball;
 	int changeY = 0;
 	int changeX = 0;
+	Typeface font;
+	
 	public MyGraphic(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 		ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+		
+		//typeface font assets
+		font = Typeface.createFromAsset(context.getAssets(), "contrast.ttf");
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
+		
 		//scale bitmap
 		Bitmap scaleBitmap = ball.createScaledBitmap(ball, 70, 70, true);
 		
@@ -48,6 +56,15 @@ public class MyGraphic extends View {
 		Paint paint = new Paint();
 		paint.setColor(Color.BLACK);
 		canvas.drawRect(rect, paint);
+		
+		//typeface font assets
+		Paint paintText = new Paint();
+		paintText.setARGB(100,  100,  100,  100);
+		paintText.setTextSize(50);
+		paintText.setTextAlign(Align.CENTER);
+		paintText.setTypeface(font);
+		
+		canvas.drawText("Hello John", canvas.getWidth()/2, 200, paintText);
 		
 		invalidate();
 	}
