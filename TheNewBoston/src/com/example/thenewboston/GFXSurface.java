58c76853b6931d7a26c2d9a67597f94dce015ef9
John Hoang <john.hoang.contact@gmail.com>
2014-06-19 14:38:07 +0700

    Add runable, animation thread

--- TheNewBoston/src/com/example/thenewboston/GFXSurface.java
+++ TheNewBoston/src/com/example/thenewboston/GFXSurface.java
@@ -2,8 +2,11 @@ package com.example.thenewboston;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class GFXSurface extends Activity {
+public class GFXSurface extends Activity implements OnTouchListener {
 
 	MyGraphicSurface graphic;
 	
@@ -12,6 +15,28 @@ public class GFXSurface extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		graphic = new MyGraphicSurface(this);
+		graphic.setOnTouchListener(this);
 		setContentView(graphic);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		graphic.SetXY(event.getX(), event.getY());
+		return false;
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		graphic.resume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		graphic.pause();
 	}
 }
--- TheNewBoston/src/com/example/thenewboston/MyGraphicSurface.java
+++ TheNewBoston/src/com/example/thenewboston/MyGraphicSurface.java
@@ -1,26 +1,82 @@
 package com.example.thenewboston;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.Paint.Align;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class MyGraphicSurface extends SurfaceView implements Runnable {
 
+	float x, y;
+	Typeface font;
 	SurfaceHolder holder;
 	Thread ourThread = null;
+	boolean isRunning = false;
 	
 	public MyGraphicSurface(Context context) {
 		// TODO Auto-generated constructor stub
 		super(context);
 		holder = getHolder();
+		font = Typeface.createFromAsset(context.getAssets(), "contrast.ttf");
+	}
+	
+	public void SetXY(float a, float b){
+		x = a;
+		y = b;
+	}
+	
+	public void resume(){
+		isRunning = true;
 		ourThread = new Thread(this);
 		ourThread.start();
+	}
+	
+	public void pause(){
+		isRunning = false;
+		while(true){
+			try {
+				ourThread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
+		ourThread = null;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		while(isRunning){
+			if(!holder.getSurface().isValid()){
+				continue;
+			}
+			
+			Canvas canvas = holder.lockCanvas();
+			canvas.drawARGB(100,  100,  100,  100);
+			
+			if(x!=0 && y!=0){
+				Bitmap test = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+				Bitmap scaleBitmap = test.createScaledBitmap(test, 70, 70, true);
+				canvas.drawBitmap(scaleBitmap, x, y, null);
+			}
+			else {
+				Paint paintText = new Paint();
+				paintText.setARGB(200,  200,  200,  200);
+				paintText.setTextSize(50);
+				paintText.setTextAlign(Align.CENTER);
+				paintText.setTypeface(font);
+				
+				canvas.drawText("Click thu xem", canvas.getWidth()/2, 200, paintText);
+			}
+			holder.unlockCanvasAndPost(canvas);
+		}
 		
 	}
-
 }
