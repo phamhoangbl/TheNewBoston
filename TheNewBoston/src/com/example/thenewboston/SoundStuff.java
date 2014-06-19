a27b899f74021e50adfc39ac235c371d6b096a80
John Hoang <john.hoang.contact@gmail.com>
2014-06-20 00:29:38 +0700

    event OnLongClick

--- TheNewBoston/src/com/example/thenewboston/SoundStuff.java
+++ TheNewBoston/src/com/example/thenewboston/SoundStuff.java
@@ -2,14 +2,17 @@ package com.example.thenewboston;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 
-public class SoundStuff extends Activity implements OnClickListener{
+public class SoundStuff extends Activity implements OnClickListener, OnLongClickListener{
 
 	SoundPool sP;
+	MediaPlayer longSong;
 	int explosion;
 	
 	@Override
@@ -22,12 +25,20 @@ public class SoundStuff extends Activity implements OnClickListener{
 		setContentView(v);
 		sP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
 		explosion = sP.load(this, R.raw.click_sound, 0);
+		longSong = MediaPlayer.create(this, R.raw.long_click_sound);
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		sP.play(explosion, 1 , 1, 0, 0, 1);
+	}
+
+	@Override
+	public boolean onLongClick(View v) {
+		// TODO Auto-generated method stub
+		longSong.start();
+		return false;
 	}
 
 }
