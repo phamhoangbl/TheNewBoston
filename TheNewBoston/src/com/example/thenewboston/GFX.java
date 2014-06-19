9616481bf0069e965ca034e5c7eaa5b2999edec0
John Hoang <john.hoang.contact@gmail.com>
2014-06-19 23:27:36 +0700

    WakeLock

--- TheNewBoston/src/com/example/thenewboston/GFX.java
+++ TheNewBoston/src/com/example/thenewboston/GFX.java
@@ -1,18 +1,36 @@
 package com.example.thenewboston;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
 public class GFX extends Activity {
 	MyGraphic myGraphic;
-
+	WakeLock wL;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
 		
+		//wake lock
+		PowerManager pM = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wL = pM.newWakeLock(PowerManager.FULL_WAKE_LOCK, "Whatever");
+		//this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		
+		
+		super.onCreate(savedInstanceState);
+		wL.acquire();
 		myGraphic = new MyGraphic(this);
 		setContentView(myGraphic);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		wL.release();
+		//getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 	
 	
