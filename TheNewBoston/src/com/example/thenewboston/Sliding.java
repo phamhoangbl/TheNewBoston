0ceb2d8f8bb1f823c430b1bf94236e183f092b00
John Hoang <john.hoang.contact@gmail.com>
2014-06-24 23:51:56 +0700

    Add sliding hander

--- TheNewBoston/src/com/example/thenewboston/Sliding.java
+++ TheNewBoston/src/com/example/thenewboston/Sliding.java
@@ -1,17 +1,19 @@
 package com.example.thenewboston;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
-import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.SlidingDrawer.OnDrawerOpenListener;
 
-public class Sliding extends Activity implements OnCheckedChangeListener, OnClickListener {
+public class Sliding extends Activity implements OnCheckedChangeListener, OnClickListener, OnDrawerOpenListener {
 
 	CheckBox cbSliable;
 	Button button1, button2, button3, button4;
+	SlidingDrawer sliding;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,9 @@ public class Sliding extends Activity implements OnCheckedChangeListener, OnClic
 		button2 = (Button) findViewById(R.id.button2);
 		button3 = (Button) findViewById(R.id.button3);
 		button4 = (Button) findViewById(R.id.button4);
+		sliding = (SlidingDrawer) findViewById(R.id.slidingDrawer1);
+		sliding.setOnDrawerOpenListener(this);
+		
 		button1.setOnClickListener(this);
 		button2.setOnClickListener(this);
 		button3.setOnClickListener(this);
@@ -38,7 +43,12 @@ public class Sliding extends Activity implements OnCheckedChangeListener, OnClic
 	@Override
 	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 		// TODO Auto-generated method stub
-		
+		if(buttonView.isChecked()){
+			sliding.lock();
+		}
+		else{
+			sliding.unlock();
+		}
 	}
 
 	@Override
@@ -46,20 +56,27 @@ public class Sliding extends Activity implements OnCheckedChangeListener, OnClic
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.button1:
-			
+			sliding.open();
 			break;
 		case R.id.button2:
 					
 			break;
 		case R.id.button3:
-			
+			sliding.toggle();
 			break;
 		case R.id.button4:
-			
+			sliding.close();
 			break;
 		default:
 			break;
 		}
+	}
+
+	@Override
+	public void onDrawerOpened() {
+		// TODO Auto-generated method stub
+		MediaPlayer media = MediaPlayer.create(this, R.raw.long_click_sound);
+		media.start();
 	}
 	
 }
