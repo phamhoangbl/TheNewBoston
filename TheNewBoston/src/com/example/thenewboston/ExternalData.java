4fce7f3d8913fefb175c8448432e95a0ddcfa035
John Hoang <john.hoang.contact@gmail.com>
2014-07-02 17:04:32 +0700

    Toast, InputStream, OutputStream, Save file to Download, Music, Picture

--- TheNewBoston/src/com/example/thenewboston/ExternalData.java
+++ TheNewBoston/src/com/example/thenewboston/ExternalData.java
@@ -1,18 +1,32 @@
 package com.example.thenewboston;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.app.Activity;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class ExternalData extends Activity {
+public class ExternalData extends Activity implements OnItemSelectedListener, OnClickListener {
 
 	TextView tvCanWrite, tvCanRead;
 	String state;
 	boolean canR, canW;
 	Spinner spinner1;
 	String[] paths = {"Music", "Picture", "DownLoad"};
-	
+	File path = null;
+	EditText etSaveAs;
+	Button btSave, btConfirm;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -50,6 +64,82 @@ public class ExternalData extends Activity {
 		
 		spinner1 = (Spinner) findViewById(R.id.spinner1);
 		spinner1.setAdapter(adapter);
+		spinner1.setOnItemSelectedListener(this);
+		
+		btConfirm = (Button) findViewById(R.id.btConfirm);
+		btSave = (Button) findViewById(R.id.btSave);
+		etSaveAs = (EditText) findViewById(R.id.etSaveAs);
+		btConfirm.setOnClickListener(this);
+		btSave.setOnClickListener(this);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		int pos = spinner1.getSelectedItemPosition();
+		switch (pos) {
+		case 0:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+			break;
+		case 1:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			break;
+		case 2:
+			path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+			break;
+		default:
+			break;
+		}
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.btSave:
+			String fileName = etSaveAs.getText().toString();
+			File file = new File(path, fileName);
+			if(canR == canW == true){
+				
+				//create parents if missing them.
+				path.mkdirs();
+				
+				try {
+					
+				InputStream is = getResources().openRawResource(R.drawable.ball);
+				OutputStream os = new FileOutputStream(file);
+				byte[] data = new byte[is.available()];
+				is.read(data);
+				os.write(data);
+				is.close();
+				os.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Toast t = Toast.makeText(ExternalData.this, "File has been created", Toast.LENGTH_LONG);
+				t.show();
+			}
+			
+			break;
+		case R.id.btConfirm:
+			btSave.setVisibility(View.VISIBLE);
+			break;
+		default:
+			break;
+		}
 	}
 	
 	
